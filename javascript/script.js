c0d9affc840ad29e696b5be63fbffdf5d79cc606
Trupthigo <mstrupthi9102@gmail.com>
2025-02-25 10:43:45 +0530

    changes

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const hamburger = document.getElementById("hamburger");
-//     const navLinks = document.getElementById("nav-links");
 
-//     hamburger.addEventListener("click", function () {
-//         navLinks.classList.toggle("show");
-//     });
-// });
 
 document.addEventListener("DOMContentLoaded", function() {
     // Enhanced Navigation Toggle
