54e91724402e796614659337760ec63aafed0e2c
Nidhilokesh <nidhilokesh600@gmail.com>
2025-02-25 10:16:13 +0530

    m

--- javascript/script.js
+++ javascript/script.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
         navLinks.classList.toggle("show");
     });
 });
+
+// const navbar = document.querySelector(".navbar");
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+
+// // Navbar scroll effect
+// window.addEventListener("scroll", () => {
+//     if (window.scrollY > 50) {
+//         navbar.classList.add("scrolled");
+//     } else {
+//         navbar.classList.remove("scrolled");
+//     }
+// });
+
+// // Toggle mobile menu
+// hamburger.addEventListener("click", () => {
+//     navLinks.classList.toggle("show");
+// });
